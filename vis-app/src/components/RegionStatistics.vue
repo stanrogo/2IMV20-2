cb49d08dd29ae5f65560e4289f586817c0087325
Stanley Clark <stanley@studyportals.com>
2018-01-26 15:09:10 +0100

    Started display of region statistics

--- vis-app/src/components/RegionStatistics.vue
+++ vis-app/src/components/RegionStatistics.vue
@@ -1,16 +1,55 @@
 <template>
-    <svg id="D3Container"></svg>
+    <v-card>
+      <h1>Total = {{totalImmigration}}</h1>
+      <svg id="D3Container"></svg>
+    </v-card>
 </template>
 
 <script>
 
-  import Refused from '../refused';
+  import RegionStatisticsD3 from '../regionStatisticsD3';
 
   export default {
     name: "region-statistics",
-    mounted(){
-      this.refused = new Refused(this.$d3);
-      this.showYear();
+    data: function(){
+      return {
+        totalImmigration: 0
+      }
+    },
+    methods: {
+      updateDisplay() {
+        this.globalStatisticsD3.year = this.year;
+        this.globalStatisticsD3.quarter = this.quarter;
+        this.totalImmigration = this.globalStatisticsD3._getTotalImmigration();
+      }
+    },
+    computed: {
+      totalImmigration: function(){
+        return 0;
+      },
+      year: function () {
+        return parseInt(this.$store.state.year);
+      },
+      quarter: function () {
+        if(!this.$store.state.quarterActive){
+          return null;
+        }
+        return (Number((this.$store.state.year % 1).toFixed(2)) + 0.25) * 4;
+      },
+    },
+    created() {
+      this.$store.watch(
+        () => this.$store.state.year,
+        () => this.updateDisplay()
+      );
+      this.$store.watch(
+        () => this.$store.state.quarterActive,
+        () => this.updateDisplay()
+      );
+    },
+    mounted() {
+      this.globalStatisticsD3 = new RegionStatisticsD3(this.$d3, 'EEA Nationals');
+      this.updateDisplay();
     }
   }
 </script>
