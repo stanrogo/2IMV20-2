7dbb58db63beca13148afc469017adf72ac8e14e
stanrogo <me@stanrogo.com>
2018-01-26 20:33:00 +0100

    Introduced nodes as pageable countries

--- vis-app/src/components/RegionStatistics.vue
+++ vis-app/src/components/RegionStatistics.vue
@@ -21,7 +21,28 @@
         </v-flex>
         <v-flex xs12 pt-0>
           <v-card color="white">
-            <v-card-text>Here it is</v-card-text>
+            <v-card-text>
+              <v-layout row wrap>
+                <v-flex xs11>
+                  <svg id="D3Container" width="100%" height="500">
+                    <svg width="100%" class="inner-svg" :y="svgOffset * -1">
+                      <text v-for="(total, name, index) in countryTotals" :x="(33 * (index  % 3) + 10) + '%'"
+                            :y="170 * Math.floor(index  / 3) + 100">{{name}}
+                      </text>
+                      <circle v-for="(total, name, index) in countryTotals" :cx="(33 * (index  % 3) + 15) + '%'"
+                              :cy="170 * Math.floor(index  / 3) + 30"
+                              :r="total/10000" fill="#4DA6FF"></circle>
+                    </svg>
+                  </svg>
+                </v-flex>
+                <v-flex xs1 class="svg-nav">
+                  <div class="svg-nav">
+                    <i class="material-icons" v-on:click="setSvgOffset(svgOffset - 510)">arrow_upward</i>
+                    <i class="material-icons" v-on:click="setSvgOffset(svgOffset + 510)">arrow_downward</i>
+                  </div>
+                </v-flex>
+              </v-layout>
+            </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
@@ -35,10 +56,10 @@
           </div>
         </v-card-title>
         <v-card-text>
-          <svg width="100%" height="500">
+          <svg width="100%" height="480">
             <g transform="translate(60,15)">
               <g>
-                <g :transform="'translate(' + (57 + (66 * (index))) + ',470)'" style="opacity: 1;"
+                <g :transform="'translate(' + (57 + (66 * (index))) + ',450)'" style="opacity: 1;"
                    v-for="(total, name, index) in totals">
                   <line x2="0" y2="0"></line>
                   <text dy=".32em" x="-3" y="0" style="text-anchor: end;">{{name}}</text>
@@ -46,9 +67,9 @@
                 <path d="M0,0H0V470H0"></path>
               </g>
               <g v-for="(total, name, index) in totals">
-                <rect class="bar" :x="(10 + (66 * (index)))" :height="total/10000" :y="450 - total/10000" fill='#5f89ad'
+                <rect class="bar" :x="(10 + (66 * (index)))" :height="total/10000" :y="430 - total/10000" fill='#5f89ad'
                       width="59"></rect>
-                <text class="label" :x="(10 + (66 * (index)))" :y="450 - total/10000 - 3" style="text-anchor: start;">
+                <text class="label" :x="(10 + (66 * (index)))" :y="430 - total/10000 - 3" style="text-anchor: start;">
                   {{total}}
                 </text>
               </g>
@@ -80,6 +101,7 @@
           'family': 0,
           'other': 0
         },
+        countryTotals: {},
         regionBreakDown: {
           ['Non-EEA Nationals']: [
             'Africa North',
@@ -104,14 +126,19 @@
           ['Other EEA and Swiss Nationals']: [
             'Europe Other'
           ]
-        }
+        },
+        svgOffset: 0,
       }
     },
     methods: {
+      setSvgOffset(offset){
+        this.svgOffset = offset;
+      },
       updateDisplay() {
         this.computeImmigrationTotal();
         this.computeRefusals();
         this.computeEntryPurpose();
+        this.computerPerCountryTotals();
       },
       computeImmigrationTotal() {
         ParseCSV.parseCSV(admissionsTotal).then(({headers, data}) => {
@@ -142,7 +169,7 @@
 
           // Get the total number of refusals for the region - the first occurrence of every label
 
-          let totals = {
+          const totals = {
             'work': 0,
             'study': 0,
             'family': 0,
@@ -197,6 +224,39 @@
 
           this.totalRefusals = total;
         });
+      },
+      computerPerCountryTotals() {
+        ParseCSV.parseCSV(purposeAdmissions).then(({headers, data}) => {
+
+          // Get all rows for the given year
+
+          let rows = data.filter(row => row[0].indexOf(this.year) !== -1);
+
+          // If quarter active, narrow down to the specific quarter
+
+          if (this.quarter > -1) {
+
+            rows = rows.filter(row => row[0].indexOf(`${this.year}`) !== -1);
+          }
+
+          // Get the total number of refusals for the region - the first occurrence of every label
+
+          const totals = {};
+
+          rows.forEach((row) => {
+
+            this.regionBreakDown[this.regionName].forEach((subRegionName) => {
+
+              if (row[1].indexOf(`${subRegionName}`) !== -1 && row[2].indexOf(`*Total`) === -1) {
+
+                totals[row[2]] = Number(parseFloat(row[3].replace(',', '')).toFixed(2));
+              }
+            });
+          });
+          console.log(totals);
+
+          this.countryTotals = totals;
+        });
       }
     },
     computed: {
@@ -233,4 +293,17 @@
 
 <style scoped>
 
+  .svg-nav{
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  i{
+    padding: 4rem 0;
+    cursor: pointer;
+  }
+  .inner-svg{
+    overflow: visible;
+  }
 </style>
